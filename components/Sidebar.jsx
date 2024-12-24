04064c4bf1c68597704286e25dc482d454e13ca5
Sohail-del <70807940+sohail-del@users.noreply.github.com>
2024-12-25 01:00:17 +0530

    Add files via upload

---
+++ components/Sidebar.jsx
@@ -0,0 +1,47 @@
+import { MenuIcon } from "lucide-react";
+import NewDocumentButton from "./ui/NewDocumentButton";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+
+function Sidebar() {
+
+  const menuOptions = (
+    <>
+    <NewDocumentButton/>
+    {/* myDocuments */}
+    {/* list */}
+    {/* shared with me  */}
+    {/* list */}
+    </>
+  )
+
+  return (
+    <div className="p-2 md:p-5 bg-gray-200 relative">
+      <div className=" md:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <MenuIcon className="p-2 hover:opacity-30 rounded-lg" size={40} />
+          </SheetTrigger>
+          <SheetContent side={"left"}>
+            <SheetHeader>
+              <SheetTitle>Menu</SheetTitle>
+              <div>{menuOptions}</div>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
+
+      <div className="hidden md:inline">{menuOptions}</div>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ components/ui/NewDocumentButton.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./button"
+
+
+const NewDocumentButton = () => {
+  return (
+    <Button>
+      New Document Button
+    </Button>
+  )
+}
+
+export default NewDocumentButton
